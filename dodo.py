1dc5dd8ed3211d367f0919998e21adf3ae5dff87
tolland1 <jaan.tollander@gmail.com>
2017-03-20 09:57:58 +0200

    deploydocs

--- dodo.py
+++ dodo.py
@@ -6,13 +6,13 @@ References:
     [#]: https://github.com/audreyr/cookiecutter-pypackage/blob/master/%7B%7Bcookiecutter.project_slug%7D%7D/Makefile
 
 Attributes:
-    PACKAGE: Package name
-    GITHUB_REPO: Github repository name
-    DOCS_DIR:
-    APIDOCSDIR:
+    AUTHOR (str): Author's name
+    PACKAGE (str): Package name
+    GITHUB_REPO (str): Github repository name
 """
 import os
 import shutil
+from functools import reduce
 from glob import iglob
 
 # Package metadata
@@ -49,67 +49,64 @@ def remove_files(*pathnames, recursive=False):
                 os.remove(path)
 
 
+def combine(*tasks):
+    """Combine actions of different tasks"""
+    return {'actions': sum((task['actions'] for task in tasks), [])}
+
+
 def task_clean_build():
     """Clean build artifacts"""
-    return {'basename': 'clean_build',
-            'actions': [(remove_files,
+    return {'actions': [(remove_files,
                          ['build/', 'dist/', '.eggs/', '*.egg-info', '*.egg'])]}
 
 
 def task_clean_pyc():
     """Clean python file artifacts"""
-    return {'basename': 'clean_pyc',
-            'actions': [
-                (remove_files, ['*.pyc', '*.pyo', '*~', '__pycache__'])]}
+    return {'actions': [
+        (remove_files, ['*.pyc', '*.pyo', '*~', '__pycache__'])]}
 
 
 def task_clean_test():
     """Clean test and coverage artifacts"""
-    return {'basename': 'clean_test',
-            'actions': [(remove_files, ['.tox/', '.coverage', 'htmlcov/'])]}
+    return {'actions': [(remove_files, ['.tox/', '.coverage', 'htmlcov/'])]}
 
 
 def task_make_docs():
     """Invoke sphinx-quickstart"""
-    return {'basename': 'make_docs',
-            'actions': [
-                ['sphinx-quickstart', SOURCEDIR, '-p', PACKAGE, '-a', AUTHOR,
-                 '-v', '"0.1"', '-l', '"en"', '--makefile', '--batchfile',
-                 '--quiet']]}
+    return {'actions': [
+        ['sphinx-quickstart', SOURCEDIR, '-p', PACKAGE, '-a', AUTHOR,
+         '-v', '"0.1"', '-l', '"en"', '--makefile', '--batchfile',
+         '--quiet']]}
 
 
 def task_clean_docs():
     """Clean documentation"""
-    return {'basename': 'clean_docs',
-            'actions': [(remove_files, [os.path.join(SOURCEDIR, BUILDDIR)])]}
+    return {'actions': [(remove_files, [os.path.join(SOURCEDIR, BUILDDIR)])]}
 
 
 def task_apidocs():
     """Build apidocs"""
-    return {'basename': 'apidocs',
-            'actions': [
-                [SPHINXAPIDOC, PACKAGE, '-o', APIDOCSDIR, '-E', '--no-toc',
-                 '--force']
-            ]}
+    return {'actions': [
+        [SPHINXAPIDOC, PACKAGE, '-o', APIDOCSDIR, '-E', '--no-toc',
+         '--force']
+    ]}
 
 
 def task_docs(builder='html'):
     """Build docs with defined ``builder``"""
-    return {'basename': 'docs',
-            'actions': [
-                [SPHINXBUILD, '-b', builder, SOURCEDIR, BUILDDIR, SPHINXOPTS]
-            ]}
+    return {'actions': [
+        [SPHINXBUILD, '-b', builder, SOURCEDIR, BUILDDIR, SPHINXOPTS]
+    ]}
 
 
-# def task_deploydocs():
-#     """Clean old docs, build apidocs, compile html and import docs into gh-pages
-#     branch"""
-#     yield task_clean_docs()
-#     yield task_apidocs()
-#     yield task_docs('html')
-#     html_docs_dir = os.path.join(SOURCEDIR, BUILDDIR, 'html')
-#     return {'basename': 'deploydocs',
-#             'actions': [['ghp-import', html_docs_dir]]}
+def task_deploydocs():
+    """Clean old docs, build apidocs, compile html and import docs into gh-pages
+    branch"""
+    html_docs_dir = os.path.join(SOURCEDIR, BUILDDIR, 'html')
+    return combine(task_clean_docs(),
+                   task_apidocs(),
+                   task_docs('html'),
+                   {'actions': [['ghp-import', html_docs_dir]]})
 
 
 def task_coverage():
